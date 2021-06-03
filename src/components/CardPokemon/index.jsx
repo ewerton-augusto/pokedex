59fe5075197e5b135409216801c1513b7b4a5d8b
Ewerton Augusto <ewerton.santos2210@gmail.com>
2021-06-03 01:31:51 -0300

    card created, showing data from the endpoint

--- src/components/CardPokemon/index.jsx
+++ src/components/CardPokemon/index.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import api from "../../services/api";
 
-const CardPokemon = () => {
+const Card = styled.div`
+  background-image: linear-gradient(to right, #f78ca0 0%, #f9748f 19%, #fd868c 60%, #fe9a8b 100%);
+  min-height: 150px;
+  padding: 10px;
+  border-radius: 15px;
+`;
+
+const CardPokemon = (props) => {
+  const [pokemon, setPokemon] = useState([]);
+  const [image, setImage] = useState("");
+  
+  useEffect(async () => {
+    const response = await api.get(props.url);
+    if(response){
+      setPokemon(response.data);
+      setImage(pokemon.sprites.front_default);
+    }
+    
+  }, []);
+
   return (
     <>
-      <h1>Card</h1>
+      <Card>
+        <img src={image} alt={pokemon.name} />
+        <p>{pokemon.id}</p>
+        <p>{pokemon.name}</p>
+      </Card>
     </>
   );
 };
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BackgroundHome, Container } from "../../styles/pages";
 import NavMenu from "../../components/NavMenu";
 
+import api from "../../services/api";
+import CardPokemon from "../../components/CardPokemon";
+import styled from "styled-components";
+
 const Home = () => {
+  const [pokemons, setPokemons] = useState([]);
+  const [previous, setPrevious] = useState("");
+  const [next, setNext] = useState("");
+
+  useEffect(() => {
+    api
+      .get("pokemon")
+      .then((response) => {
+        if (response.status === 200 && response.data) {
+          setPrevious(response.data.previous);
+          setNext(response.data.next);
+
+          setPokemons(response.data.results);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const CardList = styled.div`
+    display: grid;
+    align-items: center;
+    grid-template-columns: repeat(auto-fit, minmax(220px, 1fr));
+    grid-gap: 2rem 1rem;
+  `;
+
   return (
     <>
       <BackgroundHome>
         <Container>
           <NavMenu />
-          <h1>Home</h1>
+          <section>
+            <h1>Home</h1>
+            <input
+              type="text"
+              name=""
+              id=""
+              placeholder="Pesquisar Pokémon ID ou Nome"
+              required
+            />
+            <CardList>
+              {pokemons.map((pokemon, index) => (
+                <>
+                  <CardPokemon url={pokemon.url} key={index} />
+                </>
+              ))}
+            </CardList>
+
+          </section>
         </Container>
       </BackgroundHome>
     </>
